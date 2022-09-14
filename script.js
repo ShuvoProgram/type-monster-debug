bead5d18462474e846a6f2dc0b592be52dae3e1d
Shuvo Khan <shuvoprogramer@gmail.com>
2022-09-14 23:07:06 +0600

    fix  implement template string

--- script.js
+++ script.js
@@ -106,9 +106,9 @@ const start = () => {
 
   let count = 3;
   countdownOverlay.style.display = "flex";
-
+  // bugfix-2 --- implement template string
   const startCountdown = setInterval(() => {
-    countdownOverlay.innerHTML = '<h1>${count}</h1>';
+    countdownOverlay.innerHTML = `<h1>${count}</h1>`;
 
     // finished timer
     if (count == 0) {
